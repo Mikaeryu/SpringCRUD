134d6b63b036f40f2fbb92471acb9d59c03e09a6
Mike <hgelhem@gmail.com>
2023-03-13 12:21:37 +0300

    Добавил UserDaoImpl

---
+++ src/main/java/app/dao/UserDaoImpl.java
@@ -0,0 +1,32 @@
+package app.dao;
+
+import app.model.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao{
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public User getById(long id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, User updatedUser) {
+
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+
+    }
+
+    @Override
+    public List<User> getList() {
+        return null;
+    }
+}
