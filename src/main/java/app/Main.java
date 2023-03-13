ef2e088d6ef11c2bf0e68216ef7fe7dec8d9ac69
Mike <hgelhem@gmail.com>
2023-03-13 21:05:32 +0300

    добавил AnnotationConfigApplicationContext в Main для тестов

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,17 @@
 package app;
 
+import app.config.AppConfig;
 import app.dao.UserDaoImpl;
 import app.model.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoImpl userDao = new UserDaoImpl();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserDaoImpl userDao = context.getBean(UserDaoImpl.class);
 
         User user = new User();
         user.setAge(22);
@@ -16,5 +21,7 @@ public class Main {
 
         userDao.save(user);
         userDao.save(user);
+
+        context.close();
     }
 }
