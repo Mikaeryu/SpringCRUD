eef453a69ca50cf250aa822b37fabb3c86252de8
Mike <hgelhem@gmail.com>
2023-03-13 21:06:09 +0300

    добавил @Autowired аннотацию для entityManagerFactory

--- src/main/java/app/dao/UserDaoImpl.java
+++ src/main/java/app/dao/UserDaoImpl.java
@@ -4,15 +4,18 @@ import app.model.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao{
-    //@Autowired
-    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hibernate-mvc"); //тут лучше бы связать бином
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    private void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
 
     private EntityManager getEntityManager() {
         return entityManagerFactory.createEntityManager();
