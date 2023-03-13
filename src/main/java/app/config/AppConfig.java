311d925bd919ee5ff69a04c1714e2442b4f9867d
Mike <hgelhem@gmail.com>
2023-03-13 12:19:59 +0300

    убрал метод getEntityManager()

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,11 +1,9 @@
 package app.config;
 
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -13,11 +11,5 @@ public class AppConfig {
     @Bean
     public static EntityManagerFactory getEntityManagerFactory() {
         return Persistence.createEntityManagerFactory("hibernate-mvc");
-    }
-
-    @Bean
-    @Scope("prototype")
-    public static EntityManager getEntityManager() {
-        return getEntityManagerFactory().createEntityManager();
     }
 }
