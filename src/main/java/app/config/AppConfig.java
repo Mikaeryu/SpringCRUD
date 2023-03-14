e8b56e53d17d12f0490937e8bd366b8858def65f
Mike <hgelhem@gmail.com>
2023-03-14 21:56:42 +0300

    закоментил класс AppConfig и добавил реализацию WebConfig

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = "app")
+//@Configuration
+//@ComponentScan(basePackages = "app")
 public class AppConfig {
 
     @Bean
