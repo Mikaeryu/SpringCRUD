b9fa0573d8484a275d726e5454a9fa7a386e6d61
Mike <hgelhem@gmail.com>
2023-03-18 12:05:37 +0300

    поменял конструктор на аннотацию ломбок

--- src/main/java/app/service/UserDetailsServiceImpl.java
+++ src/main/java/app/service/UserDetailsServiceImpl.java
@@ -1,18 +1,16 @@
 package app.service;
 
 import app.dao.UserDao;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserDao userDao;
-
-    public UserDetailsServiceImpl(UserDao userDao) {
-        this.userDao = userDao;
-    }
 
     // «Пользователь» – это просто Object. В большинстве случаев он может быть
     //  приведен к классу UserDetails.
