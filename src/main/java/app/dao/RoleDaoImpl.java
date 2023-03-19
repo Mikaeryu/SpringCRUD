4ce0fb5a527e542fd43df1212120fbaa9c94785f
Mike <hgelhem@gmail.com>
2023-03-19 15:18:27 +0300

    добавил RoleDaoImpl реализацию RoleDao

---
+++ src/main/java/app/dao/RoleDaoImpl.java
@@ -0,0 +1,45 @@
+package app.dao;
+
+import app.model.Role;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RoleDaoImpl implements RoleDao{
+
+    private final EntityManager entityManager;
+
+    @Override
+    public Role saveRole(Role role) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.merge(role);
+
+        transaction.commit();
+
+        return role;
+    }
+
+    @Override
+    public Role findRole(long id) {
+        return entityManager.find(Role.class, id);
+    }
+
+    @Override
+    public void deleteRole(long id) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Role role = findRole(id);
+        if (!entityManager.contains(role)) {
+            role = entityManager.merge(role);
+        }
+        entityManager.remove(role);
+
+        transaction.commit();
+    }
+}
