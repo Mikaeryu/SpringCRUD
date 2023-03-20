b45464e4c7bd093154788bbba8e550f069d0b4be
Mike <hgelhem@gmail.com>
2023-03-20 18:39:52 +0300

    убрал лишние комменты

--- src/main/java/app/security/SecurityConfig.java
+++ src/main/java/app/security/SecurityConfig.java
@@ -24,22 +24,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()); // конфигурация для прохождения аутентификации
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // попробуйте выяснить сами, что это даёт
         http.authorizeRequests()
-                .antMatchers("/user/**").access("hasAnyRole('USER')") // разрешаем входить на /user пользователям с ролью User
+                .antMatchers("/user/**").access("hasAnyRole('USER')")
                 .antMatchers("/admin/**").access("hasAnyRole('ADMIN')")
-                .antMatchers("/").permitAll() // доступность всем
-                .and().formLogin()  // Spring сам подставит свою логин форму
-                .successHandler(successUserHandler); // подключаем наш SuccessHandler для перенеправления по ролям
+                .antMatchers("/").permitAll()
+                .and().formLogin()
+                .successHandler(successUserHandler);
     }
 
-    // Необходимо для шифрования паролей
-    // В данном примере не используется, отключен
     @Bean
     public static NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
