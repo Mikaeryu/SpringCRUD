9eda85bd2be05551676dc730c36777a4e09a4949
Mike <hgelhem@gmail.com>
2023-03-20 19:14:36 +0300

    отрефакторил UserController

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -1,24 +1,19 @@
 package app.controller;
 
 import app.model.User;
-import app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
 
     @GetMapping("/user")
-    public String showInfo(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User) authentication.getPrincipal();
-
+    public String showInfo(Model model, Authentication auth) {
+        User user = (User) auth.getPrincipal();
         model.addAttribute("user", user);
         return "user_info";
     }
