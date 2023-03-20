9f6269d5bbf1e4d6a238f7e0427302b5ae412498
Mike <hgelhem@gmail.com>
2023-03-20 18:44:30 +0300

    поменял контроллер так, что инфа о текущем пользователе показывается только на /user

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -1,20 +1,24 @@
 package app.controller;
 
+import app.model.User;
 import app.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/{id}")
-    public String showInfo(@PathVariable("id") int id, Model model) {
-        var user = userService.findUser(id);
+    @GetMapping("/user")
+    public String showInfo(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
         model.addAttribute("user", user);
         return "user_info";
     }
