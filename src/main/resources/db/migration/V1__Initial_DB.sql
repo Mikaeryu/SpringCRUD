5ff8113a431fbf3f9094d415a99b1e88969d4bce
Mike <hgelhem@gmail.com>
2023-03-23 14:12:34 +0300

    поправил sql запросы на инициализации

--- src/main/resources/db/migration/V1__Initial_DB.sql
+++ src/main/resources/db/migration/V1__Initial_DB.sql
@@ -3,7 +3,7 @@ create table if not exists users (id bigint not null auto_increment, birth_date 
     login varchar(255) not null, password varchar(255) not null, work_exp integer, primary key (id));
 create table if not exists users_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id));
 
-alter table roles add constraint UK_roles unique (name);
-alter table users add constraint UK_users unique (login);
-alter table users_roles add constraint role_id_fk foreign key (role_id) references roles (id);
-alter table users_roles add constraint user_id_fk foreign key (user_id) references users (id);
+alter table if exists roles add constraint UK_roles unique (name);
+alter table if exists users add constraint UK_users unique (login);
+alter table if exists users_roles add constraint role_id_fk foreign key (role_id) references roles (id);
+alter table if exists users_roles add constraint user_id_fk foreign key (user_id) references users (id);
