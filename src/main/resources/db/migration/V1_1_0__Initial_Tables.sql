5422fba448e4f212fd387fc97adfe61aa0bda6a2
Mike <hgelhem@gmail.com>
2023-03-29 15:31:00 +0300

    поправил sql файлы для миграции данных в БД

--- src/main/resources/db/migration/V1_1_0__Initial_Tables.sql
+++ src/main/resources/db/migration/V1_1_0__Initial_Tables.sql
@@ -1,9 +1,25 @@
-create table if not exists roles (id bigint not null auto_increment, name varchar(255), primary key (id));
-create table if not exists users (id bigint not null auto_increment, birth_date date, first_name varchar(255), last_name varchar(255),
-    login varchar(255) not null, password varchar(255) not null, work_exp integer, primary key (id));
-create table if not exists users_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id));
+create table if not exists roles (
+    id bigint not null auto_increment,
+    name varchar(255) unique,
+    primary key (id)
+);
 
-alter table roles add constraint UK_roles unique (name);
-alter table users add constraint UK_users unique (login);
-alter table users_roles add constraint role_id_fk foreign key (role_id) references roles (id);
-alter table users_roles add constraint user_id_fk foreign key (user_id) references users (id);
+create table if not exists users (
+    id bigint not null auto_increment,
+    birth_date date,
+    first_name varchar(255),
+    last_name varchar(255),
+    login varchar(255) not null unique,
+    password varchar(255) not null,
+    work_exp integer,
+    primary key (id)
+);
+
+
+create table if not exists users_roles (
+    user_id bigint not null,
+    role_id bigint not null,
+    primary key (user_id, role_id),
+    foreign key (role_id) references roles (id),
+    foreign key (user_id) references users (id)
+);
