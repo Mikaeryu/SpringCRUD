d65753f8fdac4e1f428755fb82466acc0b003b18
Mike <hgelhem@gmail.com>
2023-03-30 16:21:07 +0300

    изменил параметры методов в контроллере, так, что они теперь принимают userDto с формы

--- src/main/java/app/controller/AdminController.java
+++ src/main/java/app/controller/AdminController.java
@@ -51,29 +51,27 @@ public class AdminController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User inboundUser, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("user") UserDto inboundUserDto, @PathVariable("id") int id) {
         var existingUser = userService.findUser(id);
 
-        existingUser.setPassword(inboundUser.getPassword());
-        existingUser.setFirstName(inboundUser.getFirstName());
-        existingUser.setLastName(inboundUser.getLastName());
-        existingUser.setWorkExp(inboundUser.getWorkExp());
-        existingUser.setBirthDate(inboundUser.getBirthDate());
+        mapper.updateUserFromDto(inboundUserDto, existingUser);
 
         userService.saveUser(existingUser);
         return REDIRECT_TO_USERS;
     }
 
     @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
+    public String newUser(@ModelAttribute("user") UserDto userDto) {
         return "new";
     }
 
     @PostMapping
-    public String create(@ModelAttribute("user") User user) { //добавил тут первые три строчки, присвоение роли юзера при создании юзера
+    public String create(@ModelAttribute("user") UserDto userDto) { //добавил тут первые три строчки, присвоение роли юзера при создании юзера
         Role roleUser = roleService.findRole("ROLE_USER");
-        Set<Role> userRoleSet = user.getRoles();
+        Set<Role> userRoleSet = userDto.getRoles();
         userRoleSet.add(roleUser);
+
+        User user = mapper.toUser(userDto);
         userService.saveUser(user);
         return REDIRECT_TO_USERS;
     }
