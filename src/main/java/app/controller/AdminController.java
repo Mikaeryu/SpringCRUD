21c9f69584458f79ac21b88ef6a22436cc182dcc
Mike <hgelhem@gmail.com>
2023-03-31 10:40:53 +0300

    реализовал метод @PostMapping для создания юзера, без костылей с ролью

--- src/main/java/app/controller/AdminController.java
+++ src/main/java/app/controller/AdminController.java
@@ -2,18 +2,14 @@ package app.controller;
 
 import app.dto.UserDto;
 import app.dto.UserMapper;
-import app.model.Role;
 import app.model.User;
 import app.service.RoleService;
 import app.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-//это старый класс, тут только в некоторых местах добавления
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/users")
@@ -38,35 +34,20 @@ public class AdminController {
         return mapper.toUserDto(user);
     }
 
-    @GetMapping("/{id}/edit")
-    public String edit(@PathVariable("id") int id, Model model) {
-        model.addAttribute(userService.findUser(id));
-        return "edit";
+    @PostMapping
+    public UserDto create(@RequestBody UserDto userDto) {
+        User user = mapper.toUser(userDto);
+        userService.saveUser(user);
+        return userDto;
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public String update(@ModelAttribute("user") UserDto inboundUserDto, @PathVariable("id") int id) {
         var existingUser = userService.findUser(id);
 
         mapper.updateUserFromDto(inboundUserDto, existingUser);
 
         userService.saveUser(existingUser);
-        return REDIRECT_TO_USERS;
-    }
-
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") UserDto userDto) {
-        return "new";
-    }
-
-    @PostMapping
-    public String create(@ModelAttribute("user") UserDto userDto) { //добавил тут первые три строчки, присвоение роли юзера при создании юзера
-        Role roleUser = roleService.findRole("ROLE_USER");
-        Set<Role> userRoleSet = userDto.getRoles();
-        userRoleSet.add(roleUser);
-
-        User user = mapper.toUser(userDto);
-        userService.saveUser(user);
         return REDIRECT_TO_USERS;
     }
 
