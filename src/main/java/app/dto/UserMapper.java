9ad81885e59b060439e670f755e088395e8f5377
Mike <hgelhem@gmail.com>
2023-03-31 10:40:02 +0300

    убрал игнор трансфера ролей в методе updateUserFromDto

--- src/main/java/app/dto/UserMapper.java
+++ src/main/java/app/dto/UserMapper.java
@@ -3,7 +3,6 @@ package app.dto;
 import app.model.User;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -14,6 +13,5 @@ public interface UserMapper {
     User toUser(UserDto userDto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "roles", ignore = true)
     void updateUserFromDto(UserDto dto, @MappingTarget User entity);
 }
