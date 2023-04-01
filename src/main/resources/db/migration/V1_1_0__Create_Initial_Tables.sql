2a45b5e3166d6bf09a38fe7aeb5cc59ec14e92d3
Mike <hgelhem@gmail.com>
2023-04-01 13:13:24 +0300

    переименовал файлы миграции, добавил отдельную миграцию для Roles

--- src/main/resources/db/migration/V1_1_0__Create_Initial_Tables.sql
+++ src/main/resources/db/migration/V1_1_0__Create_Initial_Tables.sql
@@ -1,22 +1,25 @@
-create table if not exists roles (
-    id bigint not null auto_increment,
+create table if not exists roles
+(
+    id   bigint not null auto_increment,
     name varchar(255) unique,
     primary key (id)
 );
 
-create table if not exists users (
-    id bigint not null auto_increment,
+create table if not exists users
+(
+    id         bigint       not null auto_increment,
     birth_date date,
     first_name varchar(255),
-    last_name varchar(255),
-    login varchar(255) not null unique,
-    password varchar(255) not null,
-    work_exp integer,
+    last_name  varchar(255),
+    login      varchar(255) not null unique,
+    password   varchar(255) not null,
+    work_exp   integer,
     primary key (id)
 );
 
 
-create table if not exists users_roles (
+create table if not exists users_roles
+(
     user_id bigint not null,
     role_id bigint not null,
     primary key (user_id, role_id),
--- src/main/resources/db/migration/V3_1_0__Add_admin.sql
+++ src/main/resources/db/migration/V3_1_0__Add_admin.sql
@@ -1,5 +1,3 @@
 insert into users (id, login, password, work_exp) values (1, 'admin', 'admin', 0);
-insert into roles (name) values ('ROLE_USER');
-insert into roles (name) values ('ROLE_ADMIN');
 insert into users_roles (user_id, role_id) values (1, 1);
 insert into users_roles (user_id, role_id) values (1, 2);
