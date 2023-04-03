422aa0a129a29632e8d89e9f9c73f044429fc150
Mike <hgelhem@gmail.com>
2023-04-03 10:47:17 +0300

    добавил AllArgsConstructor + NoArgsConstructor для класса Roles

--- src/main/java/app/model/Role.java
+++ src/main/java/app/model/Role.java
@@ -6,7 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -14,6 +17,8 @@ import org.springframework.security.core.GrantedAuthority;
 @Setter
 @Entity
 @Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
