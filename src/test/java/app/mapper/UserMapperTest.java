31748c572f94085489079d76e3c07d4f2f502e1a
Mike <hgelhem@gmail.com>
2023-04-06 22:55:31 +0300

    покрыл тестами 2 метода, остался ещё один

--- src/test/java/app/mapper/UserMapperTest.java
+++ src/test/java/app/mapper/UserMapperTest.java
@@ -4,76 +4,97 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import app.model.Role;
 import app.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mapstruct.factory.Mappers;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Stream;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
 class UserMapperTest {
-    @Autowired
     private UserMapper userMapper;
 
-    private final static User USER_WITHOUT_ROLES = User.builder()
-            .id(1)
-            .login("user1")
-            .password("pass")
-            .firstName("User1")
-            .lastName("Userovich1")
-            .workExp(3)
-            .birthDate(LocalDate.EPOCH)
-            .roles(new HashSet<>())
-            .build();
-
-    private final static User USER_WITH_ALL_FIELDS = User.builder()
-            .id(2)
-            .login("user2")
-            .password("pass")
-            .firstName("User2")
-            .lastName("Userovich2")
-            .workExp(3)
-            .birthDate(LocalDate.MAX)
-            .roles(new HashSet<>(List.of(new Role(1L, "ROLE_USER"))))
-            .build();
-
-    private final static User USER_WITH_NULL_LOGIN = User.builder()
-            .id(3)
-            .login(null)
-            .password("pass")
-            .build();
-
-    @ParameterizedTest
-    @MethodSource("getArgumentsForMappingToUserDtoTest")
-    @Tag("toUserDto")
-    @DisplayName("All fields should be equal, when user is mapped to UserDto")
-    void AllFieldsAreEqual_whenUserIsMappedToUserDto(User user) {
-        var userDto = userMapper.toUserDto(user);
-        assertThat(userDto)
-                .isNotNull()
-                .hasFieldOrPropertyWithValue("id", user.getId())
-                .hasFieldOrPropertyWithValue("password", user.getPassword())
-                .hasFieldOrPropertyWithValue("login", user.getLogin())
-                .hasFieldOrPropertyWithValue("workExp", user.getWorkExp())
-                .hasFieldOrPropertyWithValue("roles", user.getRoles())
-                .hasFieldOrPropertyWithValue("birthDate", user.getBirthDate())
-                .hasFieldOrPropertyWithValue("lastName", user.getLastName())
-                .hasFieldOrPropertyWithValue("firstName", user.getFirstName());
+    @BeforeEach
+    void initializeUserMapper() {
+        userMapper = Mappers.getMapper(UserMapper.class);
     }
 
-    static Stream<Arguments> getArgumentsForMappingToUserDtoTest() {
+    @ParameterizedTest
+    @MethodSource("getArgumentsForMappingTest")
+    @DisplayName("All fields should be equal, when calling toUserDto() method")
+    void AllFieldsShouldBeEqual_whenUserIsMappedToUserDto(User userArg) {
+        var userDto = userMapper.toUserDto(userArg);
+
+        assertThat(userDto)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("id", userArg.getId())
+                .hasFieldOrPropertyWithValue("password", userArg.getPassword())
+                .hasFieldOrPropertyWithValue("login", userArg.getLogin())
+                .hasFieldOrPropertyWithValue("workExp", userArg.getWorkExp())
+                .hasFieldOrPropertyWithValue("roles", userArg.getRoles())
+                .hasFieldOrPropertyWithValue("birthDate", userArg.getBirthDate())
+                .hasFieldOrPropertyWithValue("lastName", userArg.getLastName())
+                .hasFieldOrPropertyWithValue("firstName", userArg.getFirstName());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArgumentsForMappingTest")
+    @DisplayName("All fields should be equal, when calling toUser() method")
+    void AllFieldsShouldBeEqual_whenUserDtoIsMappedToUser(User userArg) {
+        var userDto = userMapper.toUserDto(userArg); //тут просто перемапил обратно из аргументов, т.к. первый тест уже проверяет, что метод рабочий
+
+        var user = userMapper.toUser(userDto);
+
+        assertThat(user)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("id", userDto.getId())
+                .hasFieldOrPropertyWithValue("password", userDto.getPassword())
+                .hasFieldOrPropertyWithValue("login", userDto.getLogin())
+                .hasFieldOrPropertyWithValue("workExp", userDto.getWorkExp())
+                .hasFieldOrPropertyWithValue("roles", userDto.getRoles())
+                .hasFieldOrPropertyWithValue("birthDate", userDto.getBirthDate())
+                .hasFieldOrPropertyWithValue("lastName", userDto.getLastName())
+                .hasFieldOrPropertyWithValue("firstName", userDto.getFirstName());
+    }
+
+    @Test
+    void updateUserFromDto() {
+
+    }
+
+    static Stream<Arguments> getArgumentsForMappingTest() {
+        final User USER_WITHOUT_ROLES = User.builder()
+                .id(1)
+                .login("user1")
+                .password("pass1")
+                .firstName("User1")
+                .lastName("Userovich1")
+                .workExp(3)
+                .birthDate(LocalDate.EPOCH)
+                .build();
+
+        final User CORRECT_USER = User.builder()
+                .id(2)
+                .login("user2")
+                .password("pass2")
+                .firstName("User2")
+                .lastName("Userovich2")
+                .workExp(3)
+                .birthDate(LocalDate.MAX)
+                .roles(new HashSet<>(List.of(new Role(1L, "ROLE_USER"))))
+                .build();
+
+        final User USER_WITH_DEFAULT_FIELDS = User.builder().build();
+
         return Stream.of(
-                Arguments.of(USER_WITH_ALL_FIELDS),
-                Arguments.of(USER_WITHOUT_ROLES)
+                Arguments.of(CORRECT_USER),
+                Arguments.of(USER_WITHOUT_ROLES),
+                Arguments.of(USER_WITH_DEFAULT_FIELDS)
         );
     }
 
